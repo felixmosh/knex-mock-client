c33fcda0fcd01d28f9842edc1b250529a1362f53
Dan Michael O. Heggø <Dan.Michael.Heggo@bibsent.no>
2023-03-03 08:04:33 +0100

    Add trackerConfig

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
 import { Knex } from 'knex';
-import { Tracker } from './Tracker';
+import { Tracker, TrackerConfig } from './Tracker';
 
 export { MockClient } from './MockClient';
 export { Tracker } from './Tracker';
 export type { RawQuery, QueryMatcher, FunctionQueryMatcher } from '../types/mock-client';
 
-export function createTracker(db: Knex): Tracker {
-  const tracker = new Tracker({});
+export function createTracker(db: Knex, trackerConfig: TrackerConfig = {}): Tracker {
+  const tracker = new Tracker(trackerConfig);
   db.client.config.tracker = tracker;
   return tracker;
 }
